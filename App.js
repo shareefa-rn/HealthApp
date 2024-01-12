7040e1ca4bc0153322a28db2d78bc0f55f7c13cd
Firdous Ali <firdous.ali.tp@gmail.com>
2024-01-12 22:13:31 +0500

    fresh project working fine

--- App.js
+++
@@ -1,6 +0,0 @@
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import AppNavigation from './src/navigation/AppNavigation';
-
-export default function App() {
-  return <AppNavigation />;
-}
